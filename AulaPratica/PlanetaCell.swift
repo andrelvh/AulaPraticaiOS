32834dcb220b30f07d6d9305d35546be9f933ba4
andrelvh <de.huber@hotmail.com>
2020-06-29 09:48:39 -0300

    Adicao de células

---
+++ AulaPratica/PlanetaCell.swift
@@ -0,0 +1,14 @@
+//
+//  PlanetaCell.swift
+//  AulaPratica
+//
+//  Created by COTEMIG on 29/06/20.
+//  Copyright © 2020 Cotemig. All rights reserved.
+//
+
+import UIKit
+
+class PlanetaCell: UITableViewCell{
+    @IBOutlet weak var imagemPlaneta: UIImageView!
+    
+}
--- AulaPratica/ViewController.swift
+++ AulaPratica/ViewController.swift
@@ -8,9 +8,25 @@
 
 import UIKit
 
+struct Planet {
+    let image: String
+    let name: String
+    let about: String
+}
+
 class ViewController: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var listaPlanetas: [Planet] = [Planet(image: "Mars",
+                                          name: "Mars",
+                                          about: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war, and is often referred to as the Red Planet because the iron oxide prevalent on its surface gives it a reddish appearance that is distinctive among the astronomical bodies visible to the naked eye. Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the valleys, deserts, and polar ice caps of Earth."),
+                                   Planet(image: "Mars",
+                                          name: "Mars",
+                                          about: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war, and is often referred to as the Red Planet because the iron oxide prevalent on its surface gives it a reddish appearance that is distinctive among the astronomical bodies visible to the naked eye. Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the valleys, deserts, and polar ice caps of Earth."),
+                                   Planet(image: "Mars",
+                                          name: "Mars",
+                                          about: "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war, and is often referred to as the Red Planet because the iron oxide prevalent on its surface gives it a reddish appearance that is distinctive among the astronomical bodies visible to the naked eye. Mars is a terrestrial planet with a thin atmosphere, having surface features reminiscent both of the impact craters of the Moon and the valleys, deserts, and polar ice caps of Earth.")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +37,13 @@ class ViewController: UIViewController{
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return listaPlanetas.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Linha: \(indexPath.row)"
+        cell.textLabel?.text = listaPlanetas[indexPath.row].name
+        cell.imageView?.image = UIImage(named: listaPlanetas[indexPath.row].image)
         return cell
     }
     
