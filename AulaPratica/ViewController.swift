2f94f1ba633b6e0282736e715679870a5494a0c2
andrelvh <de.huber@hotmail.com>
2020-06-29 08:42:18 -0300

    Projeto Inicial

---
+++ AulaPratica/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AulaPratica
+//
+//  Created by COTEMIG on 22/06/20.
+//  Copyright © 2020 Cotemig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Linha: \(indexPath.row)"
+        return cell
+    }
+    
+}
